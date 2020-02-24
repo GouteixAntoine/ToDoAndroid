299b70512c5f1b9da75cccdad054678be998cd1f
GOUTEIX Antoine <ag182063@IMAC-E309-11.local>
2020-02-24 15:59:58 +0100

    Add button

--- app/src/main/java/com/android/todo/todoantoinegouteix/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/android/todo/todoantoinegouteix/tasklist/TaskListFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.todo.todoantoinegouteix.R
 import kotlinx.android.synthetic.main.fragment_task_list.*
+import java.util.*
 
 class TaskListFragment : Fragment() {
 
@@ -22,9 +23,15 @@ class TaskListFragment : Fragment() {
         recycle_view
         recycle_view.layoutManager = LinearLayoutManager(activity)
         recycle_view.adapter = TaskListAdapter(taskList)
+        floatingActionButton.setOnClickListener{
+            // Instanciation d'un objet task avec des données préremplies:
+            val task = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList.add(task)
+            (recycle_view.adapter as TaskListAdapter).notifyDataSetChanged()
+        }
     }
 
-    private val taskList = listOf(
+    private val taskList = mutableListOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
